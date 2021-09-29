5c2ce0a9b5424b7fa813e54f978048a736c2ef7a
Your Name <harrithha64@gmail.com>
2021-09-29 22:49:19 +0530

    Added team member details

--- src/Components/Team.js
+++ src/Components/Team.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {teamMembers} from '../TeamImages/memberDetails.js';
+import {teamMembers1, teamMembers2} from '../TeamImages/memberDetails.js';
 import avatar from '../TeamImages/avatar.jpg';
 import {data} from '../TeamImages/memberDetails.js';
 
@@ -10,7 +10,17 @@ class Team extends Component {
       <section id="team" style={{paddingLeft: '60px', paddingRight: '60px', color: 'white',padding: '5rem 0'}}>
       <div style={{ textAlign : 'center', color: '#fff', fontSize : '32px', marginBottom: '40px'}}>TEAM</div>
       <div style={{display: 'flex', justifyContent : 'space-evenly'}}>
-        {    teamMembers.map(data => (
+        {    teamMembers1.map(data => (
+                    <div>
+                      <img src={data.url} style={{height: '150px', width: '150px', borderRadius: '50%'}} alt="Images" /><br/>
+                      <span style={{color: 'white',textAlign: 'center', display: 'flex', justifyContent:'center'}}>{data.name}</span>
+                    </div>
+                  ))
+        }
+      </div>
+      <br /><br /><br />
+      <div style={{display: 'flex', justifyContent : 'space-evenly'}}>
+        {    teamMembers2.map(data => (
                     <div>
                       <img src={data.url} style={{height: '150px', width: '150px', borderRadius: '50%'}} alt="Images" /><br/>
                       <span style={{color: 'white',textAlign: 'center', display: 'flex', justifyContent:'center'}}>{data.name}</span>
--- src/TeamImages/memberDetails.js
+++ src/TeamImages/memberDetails.js
@@ -1,16 +1,46 @@
-import avatar from './avatar.jpg';
+import Saloni from './Saloni.jpeg';
+import Aasim from './Aasim.jpeg';
+import Atul from './Atul.jpeg';
+import Samriddha from './Samriddha.jpeg';
+import Sohan from './Sohan.jpeg';
+import Tabish from './Tabish.jpeg';
+import Yogesh from './Yogesh.jpeg';
+import Harrithha1 from './Harrithha1.png';
 
-export const teamMembers = [
+export const teamMembers1 = [
  {
-     url : avatar,
+     url : Atul,
      name : 'Atul Singh'
  },
  {
-    url : avatar,
-    name : 'Saloni Singh'
+    url : Sohan,
+    name : 'Sohan Anisetty'
 },
  {
-    url : avatar,
-    name : 'Lorem'
+    url : Saloni,
+    name : 'Saloni Singh'
+},
+{
+    url : Yogesh,
+    name : 'Yogesh Dhyani'
+}
+];
+
+export const teamMembers2 = [
+{
+   url : Aasim,
+   name : 'Aasim Ali Khan'
+},
+{
+   url : Harrithha1,
+   name : 'Harrithha B'
+},
+{
+    url : Tabish,
+    name : 'Tabish Khalid Halim'
+ },
+{
+   url : Samriddha,
+   name : 'Samriddha Chattopadhyay'
 }
 ];
